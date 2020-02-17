046cfc2a785f4c71626e8bb71a952101a02ba94a
Markov-in-Chains <61037229+Markov-in-Chains@users.noreply.github.com>
2020-02-17 23:17:50 +0530

    Add files via upload

---
+++ Packages.R
@@ -0,0 +1,35 @@
+
+library(dplyr)
+library(DataExplorer)
+library(Hmisc)
+
+
+install.packages("data.table")
+
+install.packages("knitr")
+
+install.packages("tidyverse")
+
+install.packages("lattice")
+
+install.packages("reshape2")
+
+install.packages("DataExplorer")
+
+ ## Random Forest
+install.packages("randomForest")
+library(randomForest)
+
+ ## Neural Networks
+
+install.packages("neuralnet")
+library(neuralnet)
+
+ ## Support Vector Machines
+install.packages("e1071")
+library(e1071)
+
+#Imputation of data
+
+install.packages("mice")
+library(mice)
